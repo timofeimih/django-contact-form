8286bc1fe98b3e03740923fae14108c3e2d8a3f8
Timofei <timofeimih@gmail.com>
2014-02-26 00:00:29 +0200

    changes

--- contact-form/urls.py
+++ contact-form/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.defaults import *
-import contactForm.views
+import contact-form.views
 
 urlpatterns = patterns('',
   url(r'^contacts/', 'contact-form.views.contactview'),
